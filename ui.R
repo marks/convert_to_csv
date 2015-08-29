b5b738df36774b563557326a71a91b273370f9f6
Mark Silverberg <mark.silverberg@socrata.com>
2015-08-29 13:49:52 -0400

    specify file size

--- ui.R
+++ ui.R
@@ -2,7 +2,7 @@ shinyUI(fluidPage(
   tags$head(includeScript("google-analytics.js")),
   headerPanel("Convert DBF, SAS, SPSS, and Stata files to CSV"),
   sidebarPanel(
-    helpText("Note: Only .dbf, .sas7bdat, .dta, .sav, and .csv files are supported right now"),
+    helpText("Note: Only .dbf, .sas7bdat, .dta, .sav, and .csv files up to 30 MB in size are supported right now"),
     selectInput("type", "1. Choose format", 
               choices = c("DBF","SAS","SPSS", "Stata", "CSV")),
     fileInput('file1', '2. Choose file'),
